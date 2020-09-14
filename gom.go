9bb744d706da75b26a621a4c68160d631276e1a1
Arief Setiyo Nugroho <ayiexz22@gmail.com>
2020-09-14 11:50:25 +0700

    add build filter on root

--- gom.go
+++ gom.go
@@ -3,7 +3,10 @@ package gom
 import (
 	"context"
 	"errors"
+	"fmt"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/eaciit/toolkit"
@@ -70,4 +73,130 @@ func (g *Gom) GetClient() *mongo.Client {
 // GetDatabase = Get database name
 func (g *Gom) GetDatabase() string {
 	return g.mongo.Config.Database
+}
+
+// BuildFilter = Build filter
+func (g *Gom) BuildFilter(filter *Filter) bson.M {
+	main := bson.M{}
+	inside := bson.M{}
+
+	switch filter.Op {
+	case OpAnd, OpOr:
+		insideArr := []interface{}{}
+
+		for _, fi := range filter.Items {
+			fRes := g.BuildFilter(fi)
+			insideArr = append(insideArr, fRes)
+		}
+
+		main[string(filter.Op)] = insideArr
+
+	case OpEq, OpNe, OpGt, OpGte, OpLt, OpLte, OpIn, OpNin, OpSort, OpExists:
+		inside[string(filter.Op)] = filter.Value
+		main[filter.Field] = inside
+
+	// case OpSort:
+	// 	inside.Set(string(filter.Op), filter.Value)
+	// 	main.Set(filter.Field, inside)
+
+	case OpBetween, OpRange:
+		switch filter.Value.([]interface{})[0].(type) {
+		case int:
+			gt := 0
+			lt := 0
+
+			if filter.Value != nil {
+				gt = filter.Value.([]interface{})[0].(int)
+				lt = filter.Value.([]interface{})[1].(int)
+			}
+
+			main[filter.Field] = bson.M{
+				"$gt": gt,
+				"$lt": lt,
+			}
+		case time.Time:
+			gt := time.Now()
+			lt := time.Now()
+
+			if filter.Value != nil {
+				gt = filter.Value.([]interface{})[0].(time.Time)
+				lt = filter.Value.([]interface{})[1].(time.Time)
+			}
+
+			main[filter.Field] = bson.M{
+				"$gt": gt,
+				"$lt": lt,
+			}
+		}
+
+	case OpBetweenEq, OpRangeEq:
+		switch filter.Value.([]interface{})[0].(type) {
+		case int:
+			gt := 0
+			lt := 0
+
+			if filter.Value != nil {
+				gt = filter.Value.([]interface{})[0].(int)
+				lt = filter.Value.([]interface{})[1].(int)
+			}
+
+			main[filter.Field] = bson.M{
+				"$gte": gt,
+				"$lte": lt,
+			}
+		case time.Time:
+			gt := time.Now()
+			lt := time.Now()
+
+			if filter.Value != nil {
+				gt = filter.Value.([]interface{})[0].(time.Time)
+				lt = filter.Value.([]interface{})[1].(time.Time)
+			}
+
+			main[filter.Field] = bson.M{
+				"$gte": gt,
+				"$lte": lt,
+			}
+		}
+
+	case OpStartWith:
+		main[filter.Field] = bson.M{
+			"$regex":   fmt.Sprintf("^%s.*$", filter.Value),
+			"$options": "i",
+		}
+
+	case OpEndWith:
+		main[filter.Field] = bson.M{
+			"$regex":   fmt.Sprintf("^.*%s$", filter.Value),
+			"$options": "i",
+		}
+
+	case OpContains:
+		if len(filter.Value.([]string)) > 1 {
+			bfs := []interface{}{}
+			for _, ff := range filter.Value.([]string) {
+				pfm := bson.M{}
+				pfm[filter.Field] = bson.M{
+					"$regex":   fmt.Sprintf(".*%s.*", ff),
+					"$options": "i",
+				}
+
+				bfs = append(bfs, pfm)
+			}
+			main["$or"] = bfs
+		} else {
+			main[filter.Field] = bson.M{
+				"$regex":   fmt.Sprintf(".*%s.*", filter.Value.([]string)[0]),
+				"$options": "i",
+			}
+		}
+
+	case OpNot:
+		// field := filter.Items[0].Field
+		// main.Set(field, toolkit.M{}.Set("$not", filter.Items[0].Field))
+		// toolkit.Println(toolkit.JsonStringIndent(main, "\n"))
+
+	}
+
+	return main
 }
